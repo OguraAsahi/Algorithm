c9ec4289d9ac3ed580d14f50dbb2d55878e1cdab
yifan chen （陈一帆）(BG-机票事业群) <yfan.chen@trip.com>
2021-09-01 13:57:50 +0800

    day2

---
+++ leetcode/src/hot/dp/stock_series/Stock_1.java
@@ -0,0 +1,31 @@
+package hot.dp.stock_series;
+
+/**
+ * Author：yfan.chen
+ * Date：2021-08-31 11:22 上午
+ **/
+
+/**
+ * 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+ *
+ * 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+ *
+ * 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+ */
+public class Stock_1 {
+    public int maxProfit(int[] prices){
+        int res = 0, buy = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            res = Math.max(res, prices[i] - buy);
+            buy = Math.min(buy, prices[i]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        int maxProfit = new Stock_1().maxProfit(prices);
+        System.out.println(maxProfit);
+    }
+
+}
---
+++ leetcode/src/hot/dp/stock_series/Stock_5.java
@@ -0,0 +1,8 @@
+package hot.dp.stock_series;
+
+/**
+ * Author：yfan.chen
+ * Date：2021-09-01 11:23 上午
+ **/
+public class Stock_5 {
+}
